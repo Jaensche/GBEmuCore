d96020fc0af799d6b0ce030ac1c4f28da141a5f8
Jan <jan.ertlmeier@gmail.com>
2020-03-24 13:27:05 +0100

    Write some debug info

--- GBCore/CPU.cs
+++ GBCore/CPU.cs
@@ -141,12 +141,17 @@ namespace GBCore
 
         public byte ReadMem(ushort addr)
         {
-            return RAM[addr];
+            byte data = RAM[addr];
+            Console.WriteLine($"{addr:X4} -> {data:X2}");
+
+            return data;   
         }
 
         public void WriteMem(ushort addr, byte data)
         {
             RAM[addr] = data;
+
+            Console.WriteLine($"{addr:X4} <- {data:X2}");
 
             if (addr == 0xFF02 && data == 0x81)
             {
@@ -158,43 +163,43 @@ namespace GBCore
         {
             PC = PROGMEMSTART;     // Program counter starts at 0x200
             I = 0;          // Reset index register
-            SP = 0xFFFE;         // Reset stack pointer
-            HL = 0x014D;
-            DE = 0x00D8;
-            BC = 0x0013;
-            AF = 0x01B0;
-            IME = 0;
+            //SP = 0xFFFE;         // Reset stack pointer
+            //HL = 0x014D;
+            //DE = 0x00D8;
+            //BC = 0x0013;
+            //AF = 0x01B0;
+            //IME = 0;
 
-            RAM[0xFF05] = 0x00;
-            RAM[0xFF06] = 0x00;
-            RAM[0xFF07] = 0x00;
-            RAM[0xFF10] = 0x80;
-            RAM[0xFF11] = 0xBF;
-            RAM[0xFF12] = 0xF3;
-            RAM[0xFF14] = 0xBF;
-            RAM[0xFF16] = 0x3F;
-            RAM[0xFF17] = 0x00;
-            RAM[0xFF19] = 0xBF;
-            RAM[0xFF1A] = 0x7F;
-            RAM[0xFF1B] = 0xFF;
-            RAM[0xFF1C] = 0x9F;
-            RAM[0xFF1E] = 0xBF;
-            RAM[0xFF20] = 0xFF;
-            RAM[0xFF21] = 0x00;
-            RAM[0xFF22] = 0x00;
-            RAM[0xFF23] = 0xBF;
-            RAM[0xFF24] = 0x77;
-            RAM[0xFF25] = 0xF3;
-            RAM[0xFF26] = 0xF1;
-            RAM[0xFF40] = 0x91;
-            RAM[0xFF42] = 0x00;
-            RAM[0xFF43] = 0x00;
-            RAM[0xFF45] = 0x00;
-            RAM[0xFF47] = 0xFC;
-            RAM[0xFF48] = 0xFF;
-            RAM[0xFF49] = 0xFF;
-            RAM[0xFF4A] = 0x00;
-            RAM[0xFF4B] = 0x00;
+            //RAM[0xFF05] = 0x00;
+            //RAM[0xFF06] = 0x00;
+            //RAM[0xFF07] = 0x00;
+            //RAM[0xFF10] = 0x80;
+            //RAM[0xFF11] = 0xBF;
+            //RAM[0xFF12] = 0xF3;
+            //RAM[0xFF14] = 0xBF;
+            //RAM[0xFF16] = 0x3F;
+            //RAM[0xFF17] = 0x00;
+            //RAM[0xFF19] = 0xBF;
+            //RAM[0xFF1A] = 0x7F;
+            //RAM[0xFF1B] = 0xFF;
+            //RAM[0xFF1C] = 0x9F;
+            //RAM[0xFF1E] = 0xBF;
+            //RAM[0xFF20] = 0xFF;
+            //RAM[0xFF21] = 0x00;
+            //RAM[0xFF22] = 0x00;
+            //RAM[0xFF23] = 0xBF;
+            //RAM[0xFF24] = 0x77;
+            //RAM[0xFF25] = 0xF3;
+            //RAM[0xFF26] = 0xF1;
+            //RAM[0xFF40] = 0x91;
+            //RAM[0xFF42] = 0x00;
+            //RAM[0xFF43] = 0x00;
+            //RAM[0xFF45] = 0x00;
+            //RAM[0xFF47] = 0xFC;
+            //RAM[0xFF48] = 0xFF;
+            //RAM[0xFF49] = 0xFF;
+            //RAM[0xFF4A] = 0x00;
+            //RAM[0xFF4B] = 0x00;
 
             // Clear registers V0-VF
             for (int i = 0; i < REG.Length; i++)
@@ -212,21 +217,19 @@ namespace GBCore
             // reset redraw
             RedrawFlag = false;
 
-            Console.Write("PC={0:X4}, ", PC);
+            // Load Opcode
+            byte opcode = RAM[PC];
 
+            Console.Write("{0:X4}, ", PC);            
+            Console.Write("Opcode={0:X2}, ", opcode);
+            Console.Write("A={0:X2}, ", REG[A]);
             Console.Write("B={0:X2}, ", REG[B]);
             Console.Write("C={0:X2}, ", REG[C]);
             Console.Write("D={0:X2}, ", REG[D]);
             Console.Write("E={0:X2}, ", REG[E]);
             Console.Write("H={0:X2}, ", REG[H]);
             Console.Write("L={0:X2}, ", REG[L]);
-            Console.Write("F={0:X2}, ", REG[F]);
-            Console.Write("A={0:X2}, ", REG[A]);
-
-        // Load Opcode
-        byte opcode = RAM[PC];
-
-            Console.WriteLine("Opcode={0:X2}", opcode);
+            Console.WriteLine("F={0:X2}", REG[F]);              
 
             // Decode Opcode
             // Execute Opcode
@@ -241,7 +244,7 @@ namespace GBCore
         {
             for (int i = 0; i < programCode.Length; i++)
             {
-                RAM[i] = programCode[i];
+                RAM[i] = programCode[i];               
             }
         } 
 
